e645dae76fc69f7a8815c152c1e0493c5f5b74da
yasmin yazdi <yyazdi13@gmail.com>
2020-09-27 00:14:17 -0500

    Lots of progress. Footer is up. nav and content container are now a reusable component. flavors are up and styled on the menu page

--- public/gumdrop/src/app/menu/menu.component.ts
+++ public/gumdrop/src/app/menu/menu.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, TemplateRef, ContentChild } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
 })
 export class MenuComponent implements OnInit {
+  @ContentChild('content', {static: true})
+  contentTemplate: TemplateRef<any>;
 
   constructor() { }
 
